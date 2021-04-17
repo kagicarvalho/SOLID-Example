fe7c37ea17bbb0d2d6da0c796435476cd01967ac
Kagi Adrian Garcia de Carvalho <kagi.carvalho@outlook.com>
2021-04-17 15:05:55 -0300

    Update

--- Srp/Client.cs
+++ Srp/Client.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Srp.Utils
+namespace Srp
 {
     class Client
     {
---
+++ SrpAntes/Client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SrpAntes
+{
+    class Client
+    {
+        public void CreateClient()
+        {
+
+        }
+
+        public void ReadClient()
+        {
+
+        }
+
+        public void UpdateClient()
+        {
+
+        }
+
+        public void DeleteClient()
+        {
+
+        }
+
+        public void Notify()
+        {
+            Console.WriteLine("Notificando o cliente");
+        }
+    }
+}
